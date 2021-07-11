349b95201d065067f697aa4436e74997b14181e4
Hitesh Garg <80741483+Hey-Hitesh@users.noreply.github.com>
2021-07-11 13:01:31 +0400

    Added Functionality

--- Youtube and QR/yt_qr.py
+++ Youtube and QR/yt_qr.py
@@ -3,6 +3,7 @@ import re
 import pyqrcode 
 from pyqrcode import QRCode 
 import tkinter as tk
+import webbrowser
 
 window = tk.Tk()
 window.title("Youtube Link and QR")
@@ -14,11 +15,20 @@ txt1.place(x=100,y=50)
 in1=tk.Entry(window)
 in1.place(x=100,y=80)
 
+def remove(string):
+    return string.replace(" ", "")
+
+def callback(url):
+    webbrowser.open_new(url)
 
 def proceed():
+ temp1 = (in1.get())
+ search_key = remove(temp1)
 
- search_key = (in1.get())
+ #search_key = (in1.get())
+
  # search_keyword = input("Enter the title of the video:\n")
+
  html = urllib.request.urlopen("https://www.youtube.com/results?search_query=" + search_key)
 
  video_ids = re.findall(r"watch\?v=(\S{11})", html.read().decode())
@@ -27,7 +37,9 @@ def proceed():
  temp = "https://www.youtube.com/watch?v=" + video_ids[0]
  #print("https://www.youtube.com/watch?v=" + video_ids[0])
 
- disp = tk.Label(text=temp,fg="blue")
+ disp = tk.Label(window,text=temp,fg="blue",cursor="hand2")
+ disp.pack()
+ disp.bind("<Button-1>",lambda e: callback(temp))
  disp.place(x=100,y=110)
 
  #url = pyqrcode.create(temp)
